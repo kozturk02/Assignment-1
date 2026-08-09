9ea02a08395d97f6a906517b9ff7a200d072f65e
Kayahan Ozturk <81859431+kozturk02@users.noreply.github.com>
2026-08-09 22:57:22 +1000

    Database

--- server/db.js
+++ server/db.js
@@ -1,1 +1,7 @@
 const Database = require('better-sqlite3');
+
+const db = new Database('contacts.db');
+
+db.exec('CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT, phone TEXT)');
+
+module.exports = db;
