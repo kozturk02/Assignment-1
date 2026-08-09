5d4f835ee17ebfa520e26f1f86cb689c4222d480
Kayahan Ozturk <81859431+kozturk02@users.noreply.github.com>
2026-08-09 23:45:50 +1000

    Cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./db');
 const app = express();
 const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/contacts', (req, res) => {
